99d4abbc9a5700ebbfe7a7c2f9144c26ffa5b0ff
chypot <akariyamamt@gmail.com>
2020-07-16 23:20:15 +0900

    add new message

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,17 @@
 function makeCard(message) {
-    return $('<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">' + message.user + message.datetime +'</h5><p class="card-text">'+ message.text +'</p><a href="#" class="btn">Go somewhere</a></div></div>');
+    return $('<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">' + message.user + '</h5>'
+             + '<h6 class="card-subtitle mb-2 text-muted">'+ message.datetime +'</h6>'
+             + '<p class="card-text">'+ message.text +'</p><a href="#" class="btn">Go somewhere</a></div></div>');
 }
 
-function load() {
-    console.log("loading...");
+function kickGAS(param) {
+    var dataObj =
+        (param.mode == "add") ? {count: param.count, user: param.user, text: param.text} : {count: param.count};
+    console.log(dataObj);
     $.ajax({
     type    : 'GET',
     url     : "https://script.google.com/macros/s/AKfycbwclHIIjjt2oQaoR2tF3FIHCE7bJKL712JsdYcgQPpydm7l4RE/exec",
-    data    : 'no',
+    data    : $.param(dataObj),
     dataType: 'jsonp',
     jsonpCallback   : 'jsoncallback',
     success : function(messages) {
@@ -21,28 +25,28 @@ function load() {
 
 }
 
+function load() {
+    var param = {count: 3};
+    kickGAS(param);
+}
+
+function addAndLoad(user, text) {
+    var param = {
+        count: 1,
+        mode: "add",
+        user: user,
+        text: text
+    }
+    kickGAS(param);
+}
+
 $(document).ready(function() {
     console.log("ready");
     load();
     $("#ok").click(function() {
         console.log("clicked!");
-        var url = "https://script.google.com/macros/s/AKfycbwclHIIjjt2oQaoR2tF3FIHCE7bJKL712JsdYcgQPpydm7l4RE/exec";
-
-        var datetime = (new Date()).toLocaleString();
-        var user = "";
-        var message = {
-            datetime: datetime,
-            user: user,
-            text: "TEXT",
-        };
-        $.post({
-            url: url,
-            data: JSON.stringify(message),
-            dataType: "json"
-        }).done (function(res) {
-            console.log("DONE");
-            console.log(res);
-        });
-
-        　});
+        var user = $("#add [name=user]").val();
+        var text = $("#add [name=textbody]").val();
+        addAndLoad(user, text);
+    });
 });
